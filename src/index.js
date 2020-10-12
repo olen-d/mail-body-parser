01b2258f1a17c78963ee4002a5df1ca813b6470f
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-10-11 22:49:27 -0400

    Apple Mail Parser now detects the boundaries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const appleMail = require("./apple-mail");
+
 const detectClient = message => {
   return new Promise((resolve, reject) => {
     try {
@@ -8,7 +10,7 @@ const detectClient = message => {
       if (message.includes(appleMail)) {
         client = "apple-mail";
       };
-      
+
       resolve({ client });
     } catch(error) {
       reject({ error });
@@ -16,4 +18,34 @@ const detectClient = message => {
   });
 }
 
-module.exports = { detectClient }
+const parseBody = message => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let parsedBody = null;
+
+      const { client } = await detectClient(message);
+
+      switch (client) {
+        case "apple-mail":
+          result = await appleMail.parse(message);
+          const { status, data } = result;
+          if (status !== 200)
+            {
+              // Error
+            } else {
+              parsedBody = data;
+              
+            }
+          break;
+        default:
+          // Use the generic parser
+          break;
+      }
+      resolve({ parsedBody });
+    } catch(error) {
+      reject({ error });
+    }
+  });
+}
+
+module.exports = { detectClient, parseBody }
