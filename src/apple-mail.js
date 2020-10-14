9ea7d29658c9d40444195ac0923ffe8202b78900
Salvador Dieffenbach <zogwarg@hotmail.com>
2020-10-14 00:23:02 -0400

    Apple Mail module now returns the text/plain and text/html bodies, withut the encapsulation boundaries or headers

--- src/apple-mail.js
+++ src/apple-mail.js
@@ -1,19 +1,46 @@
-const parse = message => {
+const parse = (boundary, message) => {
   return new Promise((resolve, reject) => {
     try {
-      // Parse the body
-      // Apple Mail adds a boundary in the format of --Apple-Mail-CCCCCCCC-CCCC-CCCC-CCCC-CCCCCCCCCCCC where "C" is any alphanumeric character
-      // Get the list of boundaries
-      const regex = /--Apple-Mail-[A-Za-z0-9]{8}(-[A-Zz-z0-9]{4}){3}-[A-Zz-z0-9]{12}/g;
-      const boundaries = message.match(regex);
-      message = boundaries;
 
-      // Iterate through the matches using indexOf
-      // Then begin processing...
-      // Check for plain text
+      // Get the starting positions of each body part
+      const boundaryIndices = [];
+      if (boundary) {
 
-      // Check for html
-      resolve({ status: 200, data: message });
+        // Get the starting and ending postions of each section
+        let currentIndex = 0;
+        let fromIndex = 0;
+
+        while (currentIndex !== -1) {
+          currentIndex = message.indexOf(boundary, fromIndex);
+          boundaryIndices.push(currentIndex);
+          fromIndex = currentIndex + 1;
+        }
+      }
+
+      const bodyParts = { text: null, html: null };
+      const boundaryLen = boundary.length;
+      const boundaryIndicesLen = boundaryIndices.length - 1; // The last value in boundaryIndices should always be -1
+
+      let j = 0;
+      for (let i =0; i < boundaryIndicesLen; i++) {
+        j = i + 1;
+
+        // Get the body part, minus the boundaries
+        const bodyPart = message.slice(boundaryIndices[i] + boundaryLen, boundaryIndices[j]);
+
+        // Process the body part, first get just the body without the header information
+        const headerIndex = bodyPart.indexOf("\r\n\r\n");
+        const body = bodyPart.slice(headerIndex + 4);
+
+        if (bodyPart.includes("text/plain")) {
+          bodyParts.text = body;
+        }
+        if (bodyPart.includes("text/html")) {
+          bodyParts.html = body;
+        }
+      }
+
+      resolve({ status: 200, data: bodyParts });
     } catch(error) {
       reject({ error });
     }
