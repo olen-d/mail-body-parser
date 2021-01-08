32f0b02ae6b22e833f7ffb8814b0f02f65987058
Salvador Dieffenbach <zogwarg@hotmail.com>
2021-01-07 23:59:07 -0500

    Refactored to make the generic parser more robust

--- src/generic.js
+++ src/generic.js
@@ -1,39 +1,74 @@
+const quotedPrintable = require("quoted-printable");
+const utf8 = require("utf8");
+
 const parse = (boundary, message) => {
   return new Promise((resolve, reject) => {
     try {
       const bodyParts = {};
 
-      // Check for a boundary, if it exists then deal with the multiple parts
-      if (boundary) {
-        // Multipart
-        // multipart-mixed
-        // multipart-alternative
-        bodyParts.message = message;
+      const processBodyPart = bodyPart => {
+        // Look for a content-type field in the body part
+        reContentType = /content-type:/gi;
+        if (reContentType.test(bodyPart)) {
+          // Get just the body without the header information
+          // As per the specification, headers are followed by two CRLFs
+          const headerIndex = bodyPart.indexOf("\r\n\r\n");
+          const body = bodyPart.slice(headerIndex + 4);
 
-      } else {
-        // If not, figure out what we have and then process it
-        // Get the header index
-        const headerIndex = message.indexOf("\r\n\r\n");
+          if (bodyPart.includes("text/plain")) {
+            let decoded = false;
 
-        // If no headers are present, set the body equal to the message, otherwise slice off the headers
-        const body = headerIndex === -1 ? message : message.slice(headerIndex + 4);
+            if (bodyPart.includes("Content-Transfer-Encoding: quoted-printable")) {
+              decoded = utf8.decode(quotedPrintable.decode(body));
+            } 
+            bodyParts.text = decoded;
+          }
+          if (bodyPart.includes("text/html")) {
+            let decoded = false;
 
-        // Check for Content-types
-        if (message.includes("Content-type:")) {
-          if (message.includes("Content-type: text/plain"))
-          {
-            bodyParts.text = body;
-          } else if (message.includes("Content-type: text/html")) {
-            bodyParts.html = body;
-          } else {}
+            if (bodyPart.includes("Content-Transfer-Encoding: quoted-printable")) {
+              decoded = utf8.decode(quotedPrintable.decode(body));
+            }
+            bodyParts.html = decoded;
+          }
         } else {
           // No content type, assume plain text and US ASCII
-          bodyParts.text = body;
+          decoded = utf8.decode(quotedPrintable(bodyPart)); // TODO: Update in the future to get quoted-printable from the headers
+          bodyParts.text = decoded;
         }
       }
-      resolve({ status: 200, data: bodyParts });
+
+      // Check for a boundary, if it exists then deal with the multiple parts
+      const boundaryIndices = [];
+      if (boundary) {
+        let currentIndex = 0;
+        let fromIndex = 0;
+
+        // Get the starting and ending postions of each section
+        while (currentIndex !== -1) {
+          currentIndex = message.indexOf(boundary, fromIndex);
+          boundaryIndices.push(currentIndex);
+          fromIndex = currentIndex + 1;
+        }
+
+        const boundaryLen = boundary.length;
+        const boundaryIndicesLen = boundaryIndices.length - 1; // The last value in boundaryIndices should always be -1
+
+        let j = 0;
+        for (let i =0; i < boundaryIndicesLen; i++) {
+          j = i + 1;
+
+          // Get the body part, minus the boundaries
+          const bodyPart = message.slice(boundaryIndices[i] + boundaryLen, boundaryIndices[j]);
+          processBodyPart(bodyPart);
+        }
+      } else {
+        // If a boundary is not provided assume plain text and process the message
+        processBodyPart(message);
+      }
+      resolve({ status: 200, message: "ok", data: bodyParts });
     } catch(error) {
-      reject({ error });
+      reject({ status: 500, message: "Internal Server Error", error });
     }
   });
 }
