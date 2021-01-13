79b84f1755718350edb851f442c7553b2a155d16
Salvador Dieffenbach <zogwarg@hotmail.com>
2021-01-12 20:10:00 -0500

    Unit tests for decodeBody funcion

--- src/__test__/generic.test.js
+++ src/__test__/generic.test.js
@@ -1,24 +1,44 @@
-const { detectContentType, processBodyPart } = require("../generic");
+const { decodeBody, detectContentType, processBodyPart } = require("../generic");
+
+describe("Testing decodeBody", () => { 
+  test("should decode content-transfer-encoding: quoted-printable", () => {
+    expect(decodeBody("Content-Transfer-Encoding: quoted-printable","=5BTesting=5D This is a test message.")).toBe("[Testing] This is a test message.");
+  });
+  
+  test("should return unchanged body if no content-transfer-encoding specified", () => {
+    expect(decodeBody("Content-Type: text/html; charset=utf-8","=5BTesting=5D This is a test message.")).toBe("=5BTesting=5D This is a test message.");
+  });
+  
+  test("should throw an error if content-transfer-encoding is unrecognized", () => {
+    expect(() => {
+      decodeBody("Content-Transfer-Encoding: unrecognized-type","[Testing] This is a test message.");
+    }).toThrow("Content-Transfer-Encoding not recognized. Failed to decode body");
+  });
+
+  test("should return unchanged body if no header provided", () => {
+    expect(decodeBody(false, "=5BTesting=5D This is a test message.")).toBe("=5BTesting=5D This is a test message.");
+  });
+});
 
 describe("Testing detectContentType", () => {
-  test("Detect Content-Type: text/html", () => {
+  test("should return html on detecting content-type: text/html", () => {
     expect(detectContentType("Content-Type: text/html")).toBe("html");
   });
   
-  test("Detect Content-Type: text/plain", () => {
+  test("should return text on detecting content-type: text/plain", () => {
     expect(detectContentType("Content-Type: text/plain")).toBe("text");
   });
   
-  test("Unrcognized Content-Type:", () => {
+  test("should return false for an unrcognized content-type:", () => {
     expect(detectContentType("Content-Type: text/enriched")).toBe(false);
   });
   
-  test("No Content-Type specified", () => {
+  test("should return text if no content-type specified", () => {
     expect(detectContentType("")).toBe("text");
   });
-})
+});
 
 // processBodyPart tests
-test("Return an object with text and html properties and the text or html message", () => {
+test("should return an object with text and html properties and the text or html message", () => {
   expect(processBodyPart("This is a test message.")).toEqual({text: "This is a test message."});
 });
